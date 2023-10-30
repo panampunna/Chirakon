f03e6b435f0d784c98ba1f27dcbd696f2b1565fc
john <john@ubuntu-22.043-base.john.virtualbox.org>
2023-10-30 15:42:43 +0530

    Life god plans

---
+++ Basic_START_Ubuntu.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+dd=$(date +"%d-%m-%Y  %H:%M")
+
+
+echo -e "Welcome John V ,  Today Date is ::  $dd  "
+
+
+sudo apt-get install virtualbox-guest-additions-iso
+sudo apt install -y   chromium-browser  git vim
+
+mkdir -p  /home/john/Vimal
+
+echo -e " https://github.com/panampunna/2023-ubuntu/ "
+
+git clone https://github.com/panampunna/2023-ubuntu.git
+
+git branch
+
+git checkout develop
+
+git pull
+
+ghp_kgMcHPSkfyOUXcwjOPxlE0UjKAhXjR3Iy1L
+
+
+###################3333333#####2
+
+
+
+
+
