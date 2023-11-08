a10aa5a15fcb2fc63a560c27771b19d2c9c14402
john <john@New-ubuntu22.04-35Gb.myguest.virtualbox.org>
2023-11-08 17:24:55 +0530

    NGINX SSL

---
+++ Folder_A/website_A.sh
@@ -0,0 +1,16 @@
+
+sudo apt update
+sudo apt install -y  nginx
+
+sudo cp   /home/ubuntu/Folder_A/index.html   /var/www/html
+sudo  mkdir -p /ssl
+sudo cp  /home/ubuntu/Folder_A/server.crt   /ssl
+sudo cp  /home/ubuntu/Folder_A/server.csr   /ssl
+sudo cp  /home/ubuntu/Folder_A/server.key   /ssl
+sudo cp  /home/ubuntu/Folder_A/nginx.conf    /ssl    ##   /etc/nginx/nginx.conf   path need to move  ## good to have a copy in /ssl 
+
+sudo cp  /ssl/nginx.conf   /etc/nginx/nginx.conf
+sudo service nginx restart
+
+
+
