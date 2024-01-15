431e260d76a4d8243aad87e1576e2ede867172c8
john <john@New-ubuntu22.04-35Gb.myguest.virtualbox.org>
2024-01-15 15:16:58 +0530

    git pull

---
+++ git-PULL-All.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# git config --global credential.helper store   ## to save all passwords 
+
+cd /home/john/Vimal/2023-ubuntu
+pwd 
+git checkout  develop
+git branch 
+git pull 
+
+cd /home/john/Vimal/panampunna-bitbucket-2023
+git checkout  develop
+git branch
+git pull
+
+
+cd /home/john/Vimal/gitlab_vimal
+git checkout  develop
+git branch
+git pull
---
+++ git-merge-develop.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+cd /home/john/Vimal/2023-ubuntu
+pwd 
+git checkout  main 
+git branch 
+git merge develop
+git push 
+git checkout develop
+
+cd /home/john/Vimal/panampunna-bitbucket-2023
+git checkout  main
+git branch
+git merge develop
+git push  
+git checkout  develop
+
+cd /home/john/Vimal/gitlab_vimal
+git checkout  main 
+git branch
+git merge 
+git push  
+git checkout  develop
