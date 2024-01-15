3c6fb973f8b6c384eebc7330b0e74e1b52dd25c8
john <john@New-ubuntu22.04-35Gb.myguest.virtualbox.org>
2024-01-15 14:17:23 +0530

    PW

--- Basic_START_Ubuntu.sh
+++ Basic_START_Ubuntu.sh
@@ -6,6 +6,10 @@ sudo apt  install virtualbox-guest-additions-iso
 sudo apt install -y   chromium-browser  git vim
 mkdir -p  /home/john/Vimal
 echo -e " https://github.com/panampunna/2023-ubuntu/ "
+echo -e "GIT PW  = https://drive.google.com/file/d/1Bi2bVMjTyLYeaEQNw5Cv12oMF3S_yPF1/view?usp=drive_link  " 
+echo -e "GIT PW  = GIT_Vimal.txt   google drive   " 
+
+
 
 cd /home/john/Vimal
 pwd 
@@ -30,6 +34,12 @@ cat /home/john/Vimal/2023-ubuntu/base.bash_aliases   > /home/john/.bash_aliases
 ###################3333333#####2
 
 
+git clone https://panampunna@bitbucket.org/panampunna-bitbucket/panampunna-bitbucket-2023.git 
+
+echo "PW  Google Drive   " 
+
+git clone  https://gitlab.com/vjp2/gitlab_vimal.git  
 
 
+echo "PW  Google Drive   " 
 
